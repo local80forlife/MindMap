9978c3cbd28e21328423c90e5504c3cceb526946
local80forlife <barton.andrew02@gmail.com>
2017-08-10 13:37:17 -0700

    Weird gravity problem, cards placed mid room

--- Mind Mapping/Assets/Scripts/Rayshooter.cs
+++ Mind Mapping/Assets/Scripts/Rayshooter.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Rayshooter : MonoBehaviour {
 	private Camera _camera;
 	public GameObject Note;
+	//public float WestWall = 90.0f;
 
 	// Use this for initialization
 	void Start () {
 		_camera = GetComponent<Camera> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Vector3 wallRotation;
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 point = new Vector3 (_camera.pixelWidth / 2, _camera.pixelWidth / 2, 0);
 			Ray ray = _camera.ScreenPointToRay (point);
@@ -20,9 +23,13 @@ public class Rayshooter : MonoBehaviour {
 
 			if (Physics.Raycast (ray, out hit)) {
 				Debug.Log ("Hit " + hit.point); 
+				GameObject hitWall = hit.transform.gameObject;
+				Debug.Log ("Object " + hitWall);
 				GameObject card = (GameObject)Instantiate (Note);
-				card.transform.position = hit.point;
-
+				//card.transform.position = hit.point;
+				//wallRotation = hitWall.transform.rotation;
+				//card.transform.rotation.y = wallRotation.y;
+				//Debug.Log("Rotation " + wallRotation);
 
 			}
 		}
